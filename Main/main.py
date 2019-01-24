055fd44f937a7c76a2bbbb79df6dfc5427ec6417
FrancescoGiraud0 <44125025+FrancescoGiraud0@users.noreply.github.com>
2019-01-24 15:38:41 +0100

    Added Main Robot Program

---
+++ Main/main.py
@@ -0,0 +1,21 @@
+import time
+import sys
+import serial
+import numpy as np
+import random
+from rplidar import RPLidar
+import cv2
+import config
+
+
+def objectFollower():
+    pass
+
+def trackedObjectDirection():
+    pass
+
+def lidarObjectDetector():
+    pass
+
+def main():
+    pass
