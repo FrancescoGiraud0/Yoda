eb46ddda805732245072d18fefcf77fde0a25697
MatteoAllemandi <46619805+MatteoAllemandi@users.noreply.github.com>
2019-03-21 16:25:52 +0100

    Matteo Allemandi
    FRA PRENDI QUEsto

---
+++ Training.py
@@ -0,0 +1,48 @@
+
+import sys
+import serial
+import numpy as np
+import cv2
+
+isConnected = True
+
+try:
+    arduino = serial.Serial("/dev/ttyACM0", 9600, timeout=50)
+    time.sleep(1)
+except:
+    isConnected = False
+    print("arduino non connesso")
+
+def goForward():
+    arduino.write(b'w')
+
+#Send "stop" signal to Arduino
+def stops():
+    arduino.write(b's')
+
+#Send "go backwards" signal to Arduino
+def goBackwards():
+    arduino.write(b'x')
+
+#Send "turn right" signal to Arduino
+def turnRight():
+    arduino.write(b'd')
+
+#Send "turn left" signal to Arduino
+def turnLeft():
+    arduino.write(b'a')
+
+while True:
+
+    cmd=input("comando: ")
+
+    if(cmd=='w'):
+        goForward()
+    elif(cmd=='a'):
+        turnLeft()
+    elif(cmd=='s'):
+        stops()
+    elif(cmd=='d'):
+        turnRight()
+    elif(cmd=='x'):
+        goBackwards()
